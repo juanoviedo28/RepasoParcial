59c706cf97e153a8edf3c5a7095c2c7bc88e974e
Juan Oviedo <JuanOviedo28@gmail.com>
2021-08-11 20:42:55 -0300

    creacion objeto persona completo

--- src/main/java/com/mycompany/repasoparcial2021/objetos/Persona.java
+++ src/main/java/com/mycompany/repasoparcial2021/objetos/Persona.java
@@ -13,5 +13,45 @@ public class Persona {
     
     private String documento;
     private String apellido, nombre;
+
+    public Persona() {
+    }
+
+    public Persona(String documento, String apellido, String nombre) {
+        this.documento = documento;
+        this.apellido = apellido;
+        this.nombre = nombre;
+    }
+
+    public String getDocumento() {
+        return documento;
+    }
+
+    public void setDocumento(String documento) {
+        this.documento = documento;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    @Override
+    public String toString() {
+        return "Persona{" + "documento=" + documento + ", apellido=" + apellido + ", nombre=" + nombre + '}';
+    }
+    
+    
     
 }
