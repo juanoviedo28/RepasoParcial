cf9ef46c185bfc77dafc367bbfe5cd4cf235604c
Juan Oviedo <JuanOviedo28@gmail.com>
2021-08-11 20:50:53 -0300

    creacion objeto curso completo

--- src/main/java/com/mycompany/repasoparcial2021/objetos/Curso.java
+++ src/main/java/com/mycompany/repasoparcial2021/objetos/Curso.java
@@ -15,6 +15,36 @@ public class Curso {
     
     private Date fecha;
     private String catedra;
+
+    public Curso() {
+    }
+
+    public Curso(Date fecha, String catedra) {
+        this.fecha = fecha;
+        this.catedra = catedra;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getCatedra() {
+        return catedra;
+    }
+
+    public void setCatedra(String catedra) {
+        this.catedra = catedra;
+    }
+
+    @Override
+    public String toString() {
+        return "Curso{" + "fecha=" + fecha + ", catedra=" + catedra + '}';
+    }
+    
     
     
     
