b389f3b2eabb0eb4d22dfee4642b4ec1e39010fa
Juan Oviedo <JuanOviedo28@gmail.com>
2021-08-11 02:00:24 -0300

    creacion objeto aula

---
+++ src/main/java/com/mycompany/repasoparcial2021/objetos/Aula.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.repasoparcial2021.objetos;
+
+/**
+ *
+ * @author juanoviedo28
+ */
+public class Aula {
+    
+    private int numero;
+    private int modulo;
+    
+    
+}
