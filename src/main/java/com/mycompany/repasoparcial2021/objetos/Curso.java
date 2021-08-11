4aaa09570c58ece2294c74b6b9f8a25b52f8e8f9
Juan Oviedo <JuanOviedo28@gmail.com>
2021-08-11 01:39:26 -0300

    creacion objeto curso

---
+++ src/main/java/com/mycompany/repasoparcial2021/objetos/Curso.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.mycompany.repasoparcial2021.objetos;
+
+import java.util.Date;
+
+/**
+ *
+ * @author juanoviedo28
+ */
+public class Curso {
+    
+    private Date fecha;
+    private String catedra;
+    
+    
+    
+}
