a325e70ca011da73bb33bfbe30dce4f0e111b5b1
Juan Oviedo <JuanOviedo28@gmail.com>
2021-08-11 20:47:47 -0300

    creacion objeto aula completo

--- src/main/java/com/mycompany/repasoparcial2021/objetos/Aula.java
+++ src/main/java/com/mycompany/repasoparcial2021/objetos/Aula.java
@@ -13,6 +13,36 @@ public class Aula {
     
     private int numero;
     private int modulo;
+
+    public Aula() {
+    }
+
+    public Aula(int numero, int modulo) {
+        this.numero = numero;
+        this.modulo = modulo;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public int getModulo() {
+        return modulo;
+    }
+
+    public void setModulo(int modulo) {
+        this.modulo = modulo;
+    }
+
+    @Override
+    public String toString() {
+        return "Aula{" + "numero=" + numero + ", modulo=" + modulo + '}';
+    }
+    
     
     
 }
